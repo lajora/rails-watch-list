0c2c00beb1fa0f71ef50bb76e9d56b8be83831bf
Lara Keen <lara.jr.keen@gmail.com>
2022-05-19 16:54:59 +0200

    finished backend

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = @list.bookmarks
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @list.destroy
+    redirect_to lists_path
+  end
+end
