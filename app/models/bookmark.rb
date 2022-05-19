99bc7228e91f767e29d4c77f6d05b5456404901c
Lara Keen <lara.jr.keen@gmail.com>
2022-05-19 11:58:54 +0200

    added validations and relations

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :list_id, uniqueness: { scope: :movie_id }
+  validates :comment, length: { minimum: 6 }
+end
